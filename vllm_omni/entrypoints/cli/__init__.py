296314a245cd8bc3762221403577afbef68f43a4
wangyu31577 <wangyu31577@hundsun.com>
2025-12-25 12:28:41 +0800

    add qwen-omni-7B-2.5

--- vllm_omni/entrypoints/cli/__init__.py
+++ vllm_omni/entrypoints/cli/__init__.py
@@ -1,6 +1,5 @@
 """CLI helpers for vLLM-Omni entrypoints."""
 
 from .serve import OmniServeCommand
-from vllm_omni.entrypoints.cli.benchmark.serve import OmniBenchmarkServingSubcommand
 
 __all__ = ["OmniServeCommand"]
