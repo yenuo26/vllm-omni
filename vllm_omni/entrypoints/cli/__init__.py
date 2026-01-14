7b2f144b5da6d86ec9605a43cc2bc46f1c353b14
wangyu31577 <wangyu31577@hundsun.com>
2026-01-14 11:26:51 +0800

    modify benchmark
    Signed-off-by: wangyu31577 <wangyu31577@hundsun.com>

--- vllm_omni/entrypoints/cli/__init__.py
+++ vllm_omni/entrypoints/cli/__init__.py
@@ -1,5 +1,6 @@
 """CLI helpers for vLLM-Omni entrypoints."""
 
 from .serve import OmniServeCommand
+from vllm_omni.entrypoints.cli.benchmark.serve import OmniBenchmarkServingSubcommand
 
-__all__ = ["OmniServeCommand"]
+__all__ = ["OmniServeCommand", "OmniBenchmarkServingSubcommand"]
