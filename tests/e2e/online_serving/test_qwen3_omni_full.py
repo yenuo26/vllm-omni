b7a3edc794bdae180c6837850ae7b48ed9457a90
z00939163 <zhumingjue@huawei.com>
2026-01-21 09:55:21 +0800

    删除transformers_comparison用例

--- tests/e2e/online_serving/test_qwen3_omni_full.py
+++ tests/e2e/online_serving/test_qwen3_omni_full.py
@@ -1301,61 +1301,6 @@ def test_chunked_prefill_003(test_config: tuple[str, str]) -> None:
 @pytest.mark.full
 @pytest.mark.H100_2
 @pytest.mark.parametrize("test_config", test_params)
-def test_transformers_comparison_001(test_config: tuple[str, str]) -> None:
-    """Test processing text, generating audio output via OpenAI API."""
-
-    model_path, stage_config_path = test_config
-    os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
-    model = Qwen3OmniMoeForConditionalGeneration.from_pretrained(
-        model_path,
-        dtype="auto",
-        device_map="auto",
-        attn_implementation="flash_attention_2",
-    )
-    processor = Qwen3OmniMoeProcessor.from_pretrained(model_path)
-
-    video_data_url = f"data:video/mp4;base64,{generate_synthetic_video(16, 16, 300)}"
-    image_data_url = f"data:image/jpeg;base64,{generate_synthetic_image(16, 16)}"
-    audio_data_url = f"data:audio/wav;base64,{generate_synthetic_audio(1, 5)}"
-
-    messages = messages_from_mix_data(
-        system_prompt=get_system_prompt(),
-        video_data_url=video_data_url,
-        image_data_url=image_data_url,
-        audio_data_url=audio_data_url,
-        content_text="What is recited in the audio? What is in this image? Please describe the video briefly.",
-    )
-    USE_AUDIO_IN_VIDEO = True
-    text = processor.apply_chat_template(messages, add_generation_prompt=True, tokenize=False)
-    audios, images, videos = process_mm_info(messages, use_audio_in_video=USE_AUDIO_IN_VIDEO)
-    inputs = processor(
-        text=text,
-        audio=audios,
-        images=images,
-        videos=videos,
-        return_tensors="pt",
-        padding=True,
-        use_audio_in_video=USE_AUDIO_IN_VIDEO,
-    )
-    inputs = inputs.to(model.device).to(model.dtype)
-
-    text_ids, audio = model.generate(
-        **inputs, speaker="Ethan", thinker_return_dict_in_generate=True, use_audio_in_video=USE_AUDIO_IN_VIDEO
-    )
-    text = processor.batch_decode(
-        text_ids.sequences[:, inputs["input_ids"].shape[1] :],
-        skip_special_tokens=True,
-        clean_up_tokenization_spaces=False,
-    )
-    print(f"text output is: {text}")
-    import soundfile as sf
-
-    if audio is not None:
-        sf.write("output.wav", audio.reshape(-1).detach().cpu().numpy(), samplerate=24000)
-
-@pytest.mark.full
-@pytest.mark.H100_2
-@pytest.mark.parametrize("test_config", test_params)
 def test_use_audio_in_video_001(test_config: tuple[str, str]) -> None:
     """Test processing text, generating audio output via OpenAI API."""
 
